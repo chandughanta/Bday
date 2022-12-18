4190fe4d4597e1e1fd7e7cc2ec0a5f4e2be59da4
chandughanta <66467437+chandughanta@users.noreply.github.com>
2022-12-18 11:14:36 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 30, 2022',
-  name: 'Chandu'
+  birthdate: 'Nov 19, 2023',
+  name: 'SHILPA AKKA'
 };
 
 function hideEverything() {
